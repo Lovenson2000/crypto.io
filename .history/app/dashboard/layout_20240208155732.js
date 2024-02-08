6631c012406f9ddc477134a151b236250df8947a
Lovenson2000 <beaucicotlovenson@gmail.com>
2024-02-08 23:58:40 +0800

    ..

---
+++ .history/app/dashboard/layout_20240208155732.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import DashboardNav from '../components/dashboard/DashboardNav';
+import SideNav from '../components/dashboard/Sidenav';
+
+export default function Layout({ children }) {
+    return (
+        <div className="dark:bg-slate-900 flex flex-col md:flex-row h-screen">
+            <div className="w-full flex-none md:w-64">
+                <SideNav />
+            </div>
+            <div className="md:flex-grow p-4">
+                <DashboardNav />
+                {children}
+            </div>
+        </div>
+    );
+}
